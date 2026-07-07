68d208e2fcf9131e2b2db79dac26d82e59783893
Gauravkale05 <kalegaurav1975@gmail.com>
2026-07-07 21:53:34 +0530

    problem 1193 solution on 7/7/2026s

---
+++ Leetcode sql 50/Basic Aggregate Functions/1193. Monthly Transactions I.sql
@@ -0,0 +1,15 @@
+-- Platform: LeetCode
+-- Difficulty: Easy
+-- Problem: 1193. Monthly Transactions I
+
+SELECT date_format(trans_date, '%Y-%m') AS month, country,
+COUNT(*) as trans_count,
+SUM(CASE
+    WHEN state = 'approved' THEN 1 ELSE 0
+END) AS approved_count,
+SUM(amount) AS trans_total_amount,
+SUM(CASE
+    WHEN state = 'approved' THEN amount ELSE 0
+END) AS approved_total_amount
+FROM Transactions 
+GROUP BY date_format(trans_date, '%Y-%m'), country;
